b6a29467e4fc87282167bf25b6f3f9c0537a2d72
MikeGawi <gawi@poczta.fm>
2020-12-24 23:17:56 +0100

    Added service logs and rotation

--- ePiframe_service.py
+++ ePiframe_service.py
@@ -3,6 +3,7 @@
 import sys, time, sched, os, signal
 import subprocess
 from misc.daemon import daemon
+from misc.logs import logs
 from misc.constants import constants
 from modules.configmanager import configmanager
 from modules.timermanager import timermanager
@@ -10,9 +11,9 @@ from modules.intervalmanager import intervalmanager
 
 class service(daemon):
 	
-	__sched = sched.scheduler(time.time, time.sleep)
 	__config_path = os.path.dirname(os.path.realpath(__file__))
 	__script = [sys.executable, os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ePiframe.py')]
+	__sched = sched.scheduler(time.time, time.sleep)
 	
 	def run(self):
 		try:
@@ -21,24 +22,31 @@ class service(daemon):
 			logs.show_log("Error loading {} configuration file! {}".format(constants.CONFIG_FILE ,e))
 			raise
 		
+		self.__logging = logs(config.get('log_files'))
+		self.__logging.log("--------ePiframe Service: STARTED", silent=True)
 		interval = intervalmanager(config.get('interval_mult_file'))
 		interval.remove()
 		
-		self.__sched.enter(0, 1, self.task, (self.__sched,))
+		self.__sched.enter(10, 1, self.task, (self.__sched,))
 		self.__sched.run()
 		
+		while True:
+			time.sleep(1)
+	
 	def task(self, scheduler):
 		try:
+			#reload config
 			config = configmanager(os.path.join(self.__config_path, constants.CONFIG_FILE))
 		except Exception as e:
-			logs.show_log("Error loading {} configuration file! {}".format(constants.CONFIG_FILE ,e))
+			self.__logging.log("Error loading {} configuration file! {}".format(constants.CONFIG_FILE ,e))
 			raise
 			
 		timer = timermanager(config.get('start_times').split(','),config.get('stop_times').split(','))
+		inter = -1
 		
 		if config.getint('interval_mult') == 1:
 			interval = intervalmanager(config.get('interval_mult_file'))
-			inter = -1
+			
 			try:
 				inter = interval.read()
 			except Exception:
@@ -47,16 +55,20 @@ class service(daemon):
 			if inter > 0:
 				inter -= 1
 				interval.save(inter)
+				self.__logging.log("Interval multipicated for current photo", silent=True)
 			else:
 				interval.remove()
-				if timer.should_i_work_now():
-					subprocess.Popen(self.__script)
-		else:
+				inter = -1
+		
+		if inter < 0:
 			if timer.should_i_work_now():
+				self.__logging.log("--------ePiframe Service: Starting ePiframe script", silent=True)
 				subprocess.Popen(self.__script)
-			
-		frameTime = config.get('slide_interval')
-		self.__sched.enter(int(frameTime), 1, self.task, (scheduler,))
+			else:
+				self.__logging.log("--------ePiframe Service: Off hours - sleeping", silent=True)
+
+		frameTime = config.get('slide_interval')			
+		self.__sched.enter(int(frameTime), 1, self.task, (scheduler,))	
 			
 if __name__ == "__main__":
 	daemon = service('/tmp/ePiframe-service.pid', os.path.dirname(os.path.realpath(__file__)))
