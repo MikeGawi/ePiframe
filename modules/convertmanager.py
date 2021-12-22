f5e7e3c4f64f9ee0d7e0fc0efc608cda15a7c636
Michał Gawinecki <michal.gawinecki@gmail.com>
2021-12-22 20:37:35 +0100

    Improve photo convert time by working on target size #23

--- modules/convertmanager.py
+++ modules/convertmanager.py
@@ -1,6 +1,12 @@
 import sys, os
 import subprocess
 
+#aspect = orgwidth/orgheight
+#scriptheight = 800 / aspect
+#scale = aspect * 10
+#usr/bin/convert photo.jpg -limit thread 1 \( +clone \( -clone 0 -gravity center -sample scriptheight -scale scale% -resize 800x -crop 800x+0+0 +repage \) \( -clone 0 -sample x480 \) -delete 0 -gravity center -compose over -composite -auto-gamma -channel rgb -auto-level -normalize -brightness-contrast 0,10 -dither FloydSteinberg -remap pattern:gray50 -background white -gravity center -extent 800x480 -type bilevel -write ./photo2.bmp \) NULL:
+
+
 class convertmanager:
 	
 	__ERROR_VALUE_TEXT = 'Configuration background_color should be one of {}'
@@ -8,24 +14,23 @@ class convertmanager:
 	__INVERT_FLAG = "-negate"
 	__ROTATE_CODE = '-rotate 90'
 	__BACK_COLORS = ["white", "black", "photo"]
-	__SCALE_HORIZON = '-scale 2.51%'
-	__SCALE_VERTIC = '-scale 4%'
 	
 	__GET_PHOTO_SIZE_CODE = '{} {} -format "%w,%h" info:'
 	
 	#Don't use blur! Blur will kill Raspberry Pi Zero.
 	#Resizing to huge and then scaling to small will add some blur and it's 10x faster than blur operation.
-	__PHOTO_BACK_CODE = '( -clone 0 -gravity center ' + __SCALE_HORIZON + ' -resize 4000% -crop {}x{}+0+0 +repage ) ( -clone 0 -sample {}x{} ) -delete 0 -gravity center -compose over -composite'
-	
+	__PHOTO_BACK_CODE = '( -clone 0 -gravity center -sample x{} -scale {}% -resize {}x -crop {}x+0+0 +repage ) ( -clone 0 -sample x{} ) -delete 0 -gravity center -compose over -composite '
+	__PHOTO_RESIZE_CODE = '-sample {}x{} '
+		
 	#options for ImageMagick converter
 	#https://legacy.imagemagick.org/Usage/quantize/
 	__CONVERT_OPTIONS = {
-		'1'	:	'{} {} -limit thread 1 {} ( +clone {}{}-brightness-contrast 0,10 -sample {}x{} -dither FloydSteinberg -remap pattern:gray50 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
-		'2'	:	'{} {} -limit thread 1 {} ( +clone {}{}-sample {}x{} -dither FloydSteinberg {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
-		'3'	:	'{} {} -limit thread 1 {} ( +clone {}{}-sample {}x{} -dither FloydSteinberg -remap pattern:gray50 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
-		'4'	:	'{} {} -limit thread 1 {} ( +clone {}{}-sample {}x{} -dither FloydSteinberg -ordered-dither o4x4 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
-		'5'	:	'{} {} -limit thread 1 {} ( +clone {}{}-sample {}x{} {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
-		'6'	:	'{} {} -limit thread 1 {} ( +clone {}{}-sample {}x{} -colors 2 +dither {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:'
+		'1'	:	'{} {} -limit thread 1 {} ( +clone {}{}-brightness-contrast 0,10 -dither FloydSteinberg -remap pattern:gray50 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
+		'2'	:	'{} {} -limit thread 1 {} ( +clone {}{}-dither FloydSteinberg {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
+		'3'	:	'{} {} -limit thread 1 {} ( +clone {}{}-dither FloydSteinberg -remap pattern:gray50 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
+		'4'	:	'{} {} -limit thread 1 {} ( +clone {}{}-dither FloydSteinberg -ordered-dither o4x4 {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
+		'5'	:	'{} {} -limit thread 1 {} ( +clone {}{}{}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:',
+		'6'	:	'{} {} -limit thread 1 {} ( +clone {}{}-colors 2 +dither {}-background {} -gravity center -extent {}x{} -type bilevel -write {} ) {} NULL:'
 	}
 	
 	__THUMB_SIZE = '200x120'
@@ -61,20 +66,18 @@ class convertmanager:
 		if back == "photo":
 			back = self.__BACK_COLORS[0]
 			#this takes more time to progress		
-			aspectratio = width / height
-			newheight = int(int(origwidth) / aspectratio)
-			#space at the end as this flag is optional
-			code = self.__PHOTO_BACK_CODE.format(origwidth, newheight, origwidth, newheight) + " "
+			aspectratio = int(origwidth) / int(origwidth)
+			newheight = int(width / aspectratio)
+			scale = round(aspectratio * 10.0, 2)
+			code = self.__PHOTO_BACK_CODE.format(newheight, scale, width, width, height)
 			
-			#bigger scale to cover whole screen in vertical position
-			if horizontal == 0: code = code.replace(self.__SCALE_HORIZON, self.__SCALE_VERTIC)
 		else:
-			code = ''
+			code = self.__PHOTO_RESIZE_CODE.format(width, height)
 		
 		thumb1st = self.__THUMB_1ST_PART.format(width, height, width, height, thumborgfile)
 		thumb2nd = self.__THUMB_2ND_PART.format(thumbconvfile)
 		
-		ret = self.__CONVERT_OPTIONS[str(option)].format(bin, srcfile, thumb1st, rotate, code, width, height, negate, back, width, height, target, thumb2nd)
+		ret = self.__CONVERT_OPTIONS[str(option)].format(bin, srcfile, thumb1st, rotate, code, negate, back, width, height, target, thumb2nd)
 		print (ret.replace('(','\(').replace(')','\)'))
 		return ret
 	
